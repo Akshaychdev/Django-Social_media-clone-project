00caf048ebd8e5a7028f7877f9a19040be689d8e
Akshay-ch-dj <akshaych203@gmail.com>
2020-05-21 13:57:54 +0530

    On groups app, added urls.py, registered models to admin

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+# Setting up the Group model and the GroupMember model
+
+# TabularInline class in admin, it allows one to utilise the admin interface in Django
+# With the ability to edit models on the same page as the parent model(here Group is parent model)
+# If one click the Group model in admin page, he can edit the GroupMember model too
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+# No need to register GroupMember as it is inline to parent
+
+admin.site.register(models.Group)
--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -5,5 +5,12 @@ from . import views
 app_name = 'groups'
 
 urlpatterns = [
+    # List Group page, homepage of groups app
     path('', views.ListGroups.as_view(), name='all'),
+    # The create-group view
+    path('new/', views.CreateGroup.as_view(), name='create'),
+    # Detail view, this time not with the primary key but with the more user readable slug
+    # <slug> --> slugifies the actual group name
+    path('posts/in/<slug>/', views.SingleGroup.as_view(), name='single'),
+    # Views for joining and leaving group
 ]
